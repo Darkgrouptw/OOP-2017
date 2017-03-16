bb26bd413298a67e977bff235d5106b9a9d2300e
Darkgrouptw <darkgrouptw@gmail.com>
2017-03-16 15:24:48 +0800

    第一題做完囉~~

--- HW4 03-16/HW4_Q1/HW4_Q1/main.cpp
+++ HW4 03-16/HW4_Q1/HW4_Q1/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void main()
+void CountNOrder(int *years, const int Count)
 {
+	double NOrder = log2(2);
+	int CurrentOrder = 3;
+	double CPU = 4;
+	for (int i = 0; i < Count; i++)
+	{
+		while (NOrder <= CPU)
+		{
+			NOrder += log2(CurrentOrder++);
+		}
+		CPU *= 2;
+		years[i] = CurrentOrder - 2;
+	}
+}
 
+int main()
+{
+	int num;
+
+	// 前處理
+	const int YearCount = (220 - 190 + 1);
+	int *years = new int[YearCount];
+	CountNOrder(years, YearCount);
+
+	while (cin >> num)
+	{
+		// 預計要次方的間隔
+		int CountYear = num / 10 - 190;
+		cout << years[CountYear] << endl;
+
+	}
+	return 0;
 }
